2350cdf4ab96eaa0a16a9431e326b1f25a3ff241
Pragya Gaur <gaur273pragya@gmail.com>
2024-08-06 23:43:32 +0530

    bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -85,16 +85,6 @@ function App() {
     );
   };
 
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://apply.devfolio.co/v2/sdk.js";
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
   const scrollToSection = (id) => {
     const element = document.getElementById(id);
     if (element) {
